efade53a6e371b1c74c4b412c2ab7f2adad38ebb
aravindanbalan <aravindanbalan@users.noreply.github.com>
2014-03-06 19:52:54 -0800

    Update AnonymousRecv.h

--- AnonymousRecv.h
+++ AnonymousRecv.h
@@ -1,27 +1,60 @@
-static void anonymousReceiver (Ptr<Socket> socket,int senderNode, std::string sourceControl, std::string sourceMessageId, std::string sourceMessage)
+void DCNET(Ptr<Socket> socket, int numRounds);
+
+static void SendMessageUsingDCNET (Ptr<Socket> socket,int senderNode, std::string sourceControl, std::string sourceMessageId, std::string sourceMessage)
 {
 	ApplicationUtil *appUtil = ApplicationUtil::getInstance();
-	std::cout<<"Inside Anonymous receiver code\n";
 	sender = senderNode;
+	if(sourceControl == INITIATE_MESSAGE)
+	{
+		
+		std::cout<<"Inside Anonymous receiver code\n";
+		
+	
+		//Message = message;
 
-	MessageLength = (int)strlen(sourceMessage.c_str()) ;
-    	std::cout<<"Message length:"<<MessageLength<<"\n";
-	//Message = message;
-
-	std::string senderPublicKey, encodedPublicKey;
-    	appUtil->getShortLivedPublicKeyFromMap(senderNode).Save(CryptoPP::StringSink(senderPublicKey).Ref());
+		std::string senderPublicKey, encodedPublicKey;
+	    	appUtil->getShortLivedPublicKeyFromMap(senderNode).Save(CryptoPP::StringSink(senderPublicKey).Ref());
 
 	
-	StringSource( senderPublicKey, true,
-	    new HexEncoder(
-		new StringSink( encodedPublicKey )
-	    ) // HexEncoder
-	);
+		StringSource( senderPublicKey, true,
+		    new HexEncoder(
+			new StringSink( encodedPublicKey )
+		    ) // HexEncoder
+		);
 
-	std::cout<<"Public key of node A : "<<encodedPublicKey<<"\n";
-	Message = encoded_message_initiate(sourceControl, sourceMessageId, sourceMessage, encodedPublicKey );
-	//DCNET(socket, 0);
-	std::cout<<"Message in binary format\n";
-	//std::cout<<Message;
-	//decode_binary(Message.c_str());  - correctly working
+		std::cout<<"Public key of node A : "<<encodedPublicKey<<"\n";
+
+		Message = encoded_message_initiate(sourceControl, sourceMessageId, sourceMessage, encodedPublicKey );
+
+		MessageLength = (int)strlen(Message.c_str()) ;
+		totalTimeStart = Simulator::Now();  	
+		DCNET(socket, 0); //- working
+		std::cout<<"Message in binary format\n";
+		//std::cout<<Message;
+		//decode_binary(Message.c_str());  - correctly working
+		//decode_binary(sharedMessage.str().c_str()); - is also working
+
+	}
+
+	if(sourceControl == MESSAGE_SET)
+	{
+		
+		// Generate a random key	
+		AESrnd.GenerateBlock( AESkey, AESkey.size() );
+		std::string AESKey_String = hexStr(AESkey.BytePtr(), AESkey.SizeInBytes());
+
+
+		std::string sendPublicKey, encodedPublicKey;
+		//node A public key
+		appUtil->getShortLivedPublicKeyforMsgIdFromMap(senderNode,atoi(sourceMessageId.c_str())).Save(CryptoPP::StringSink(sendPublicKey).Ref());
+
+		StringSource( sendPublicKey, true,
+		    new HexEncoder(
+			new StringSink( encodedPublicKey )
+		    ) // HexEncoder
+		);
+
+		Message = encoded_message_set(sourceControl, sourceMessageId, AESKey_String, encodedPublicKey);
+		MessageLength = (int)strlen(Message.c_str()) ;		
+	}
 }
