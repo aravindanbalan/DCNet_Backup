7da79d7ef4a9ccb961e59b5d102f3df6847005a1
aravindanbalan <aravindanbalan@users.noreply.github.com>
2014-03-09 17:39:49 -0700

    Update AnonymousRecv.h

--- AnonymousRecv.h
+++ AnonymousRecv.h
@@ -6,15 +6,8 @@ static void SendMessageUsingDCNET (Ptr<Socket> socket,int senderNode, std::strin
 	sender = senderNode;
 	if(sourceControl == INITIATE_MESSAGE)
 	{
-		
-		std::cout<<"Inside Anonymous receiver code\n";
-		
-	
-		//Message = message;
-
 		std::string senderPublicKey, encodedPublicKey;
 	    	senderPublicKey = appUtil->getShortLivedPublicKeyFromMap(senderNode);
-		
 	
 		StringSource( senderPublicKey, true,
 		    new HexEncoder(
@@ -22,23 +15,13 @@ static void SendMessageUsingDCNET (Ptr<Socket> socket,int senderNode, std::strin
 		    ) // HexEncoder
 		);
 
-		std::cout<<"Public key of node A : "<<encodedPublicKey<<"\n";
-
 		Message = encoded_message_initiate(sourceControl, sourceMessageId, sourceMessage, encodedPublicKey );
-
 		MessageLength = (int)strlen(Message.c_str()) ;
 		totalTimeStart = Simulator::Now();  	
-		DCNET(socket, 0); //- working
-		//std::cout<<"Message in binary format\n";
-		//std::cout<<Message;
-		//decode_binary(Message.c_str());  - correctly working
-		//decode_binary(sharedMessage.str().c_str()); - is also working
-
+		DCNET(socket, 0);
 	}
-
 	if(sourceControl == MESSAGE_SET)
-	{
-		 
+	{	 
 		// Generate a random key	
 		AESrnd.GenerateBlock( AESkey, AESkey.size());
 		// Generate a random IV
@@ -46,34 +29,23 @@ static void SendMessageUsingDCNET (Ptr<Socket> socket,int senderNode, std::strin
 
 		std::string AESKey_String = hexStr(AESkey.BytePtr(), AESkey.SizeInBytes());
 		appUtil->putUsedAESKeyInMap(senderNode,AESkey);
-		std::cout<<"************Actual sent AES key : "<<AESKey_String<<"\n";
 		std::string sendPublicKey, encodedPublicKey;
 		//node A public key
 		sendPublicKey = appUtil->getShortLivedPublicKeyforMsgIdFromMap(senderNode,atoi(sourceMessageId.c_str()));
 	
-/*	StringSource( sendPublicKey, true,
-		    new HexEncoder(
-			new StringSink( encodedPublicKey )
-		    ) // HexEncoder
-		);
-*/
-	      std::cout<<"***************Sent message id : "<<sourceMessageId<<"\n";
 		Message = encoded_message_set(sourceControl, sourceMessageId, AESKey_String, sendPublicKey);
 		MessageLength = (int)strlen(Message.c_str()) ;	
-		std::cout<<"message : "<<Message<<"\n";
-		sharedMessage.str("");
-		DCNET(socket, 0);	
 		
-				
+		sharedMessage.str("");
+		DCNET(socket, 0);				
 	}
 	if(sourceControl == MESSAGE_REPLY)
 	{
 		SecByteBlock senderAESKey = appUtil->getReceivedAESKeyForMsgId(senderNode, sourceMessageId);
 		Message = encoded_message_reply(sourceControl, sourceMessageId, sourceMessage, senderAESKey);
 		MessageLength = (int)strlen(Message.c_str()) ;	
-		std::cout<<"message : "<<Message<<"\n";
+		
 		sharedMessage.str("");
 		DCNET(socket, 0);
-		
 	}
 }
